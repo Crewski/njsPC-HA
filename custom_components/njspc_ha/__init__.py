b830f262ecb3204489029c791902b632780e74e0
Crewski <davidnaylor04@gmail.com>
2023-02-08 20:15:02 -0500

    Change to async_forward_entry_setups in init

--- custom_components/njspc_ha/__init__.py
+++ custom_components/njspc_ha/__init__.py
@@ -60,7 +60,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     async def _async_sio_close(_: Event) -> None:
         await coordinator.sio_close()
